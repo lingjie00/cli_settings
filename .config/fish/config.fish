a1adf8c4fcedf9d38a84ab0c845eea151b99fdf9
ling <64540764+lingjie00@users.noreply.github.com>
2022-02-01 23:52:51 +0800

    fix fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
+# conda
 switch (uname)
     case Darwin
         eval /Users/lingjie/miniconda3/bin/conda "shell.fish" "hook" $argv | source
     case Linux
         eval /home/ling/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 end
-# <<< conda initialize <<<
 
 # vim mode
 fish_vi_key_bindings

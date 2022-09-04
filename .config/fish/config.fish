a862d8aeaedc509e2a8137593750eb80d04ce0fe
ling <64540764+lingjie00@users.noreply.github.com>
2022-09-04 15:13:09 +0800

    remove fish config un-needed items

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# conda
-switch (uname)
-    case Darwin
-        eval /Users/lingjie/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-        fish_add_path /Users/lingjie/Documents/scripts
-        set -x JDTLS_HOME ~/.local/share/nvim/lsp_servers/jdtls
-    case Linux
-        eval /home/ling/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-
 # vim mode
 fish_vi_key_bindings
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-

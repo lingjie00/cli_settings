9d6462dffece11bb8b07f34bb05ed81260201533
ling <64540764+lingjie00@users.noreply.github.com>
2024-04-12 16:40:01 +0800

    feat(copilot): add internet check for copilot setup
    This commit adds a condition to check if the internet is available
    before setting up GitHub Copilot and CopilotChat. This prevents
    potential errors when the internet is not available.

--- .config/nvim/plugin/copilot.lua
+++ .config/nvim/plugin/copilot.lua
@@ -1,9 +1,11 @@
--- config for GitHub Co-pilot
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+if Internet == 1 then
+    -- config for GitHub Co-pilot
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
--- chat UI
-local chat = require("CopilotChat")
-chat.setup( {
-    debug = false,
-})
+    -- chat UI
+    local chat = require("CopilotChat")
+    chat.setup( {
+        debug = false,
+    })
+end

b37b589fe813371800d2318aca9151d7348854e4
ling <64540764+lingjie00@users.noreply.github.com>
2024-04-17 14:06:18 +0800

    feat(nvim): simplify LSP keymap setup
    The LSP keymap setup has been simplified by removing the on_attach
    callback. The keymaps are now set directly without the need for
    buffer-specific options. This change makes the code cleaner and easier
    to understand.

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -12,22 +12,16 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gS", require('telescope.builtin').lsp_workspace_symbols, opts)
-    vim.keymap.set("n", "gs", require('telescope.builtin').lsp_document_symbols, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "gn", vim.lsp.buf.rename, opts)
-end)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gS", require('telescope.builtin').lsp_workspace_symbols)
+vim.keymap.set("n", "gs", require('telescope.builtin').lsp_document_symbols)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gn", vim.lsp.buf.rename)
 vim.keymap.set("n", "ge", vim.diagnostic.open_float)
-
-lsp.setup()
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 vim.diagnostic.config({
     virtual_text = false,

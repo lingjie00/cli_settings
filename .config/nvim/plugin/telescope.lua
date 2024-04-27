31642c6d55bc50b3e262ae76623b667ef6ea5751
ling <64540764+lingjie00@users.noreply.github.com>
2024-04-27 14:55:40 +0800

    feat(telescope): add new keybindings
    This commit introduces new keybindings for the telescope plugin in
    Neovim. The new keybindings include:
    
    - <leader>b for buffers
    - <leader>t for help tags
    - <leader>m for marks
    - <leader>e for built-in commands
    
    This change enhances the user's ability to navigate and perform
    operations within Neovim.

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -21,10 +21,13 @@ require('telescope').setup({
 
 vim.keymap.set('n', '<leader>F', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.git_files, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>t', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>m', builtin.marks, {})
 vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>;', builtin.live_grep, {})
 vim.keymap.set({ 'n', 'v' }, '<leader>r', builtin.commands, {})
-vim.keymap.set({ 'n', 'v' }, '<leader>R', builtin.builtin, {})
+vim.keymap.set({ 'n', 'v' }, '<leader>e', builtin.builtin, {})
 
 require("telescope").load_extension("ui-select")
 require('telescope').load_extension('dap')

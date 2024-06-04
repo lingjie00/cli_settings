f3ebffa2efb6d4a73a9e975958b93fe04d937e77
ling <64540764+lingjie00@users.noreply.github.com>
2024-06-05 00:23:50 +0800

    feat: remove version-dependent key bindings
    The key bindings for scrolling up and down were previously dependent on
    the qutebrowser version. This commit removes the version check and the
    old key bindings, simplifying the configuration. This change is
    compatible with qutebrowser versions 2.5.4 and above.

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,11 +1,5 @@
 import dracula.draw
 
-try:
-    from packaging.version import Version
-except ImportError:
-    from pkg_resources import parse_version as Version
-
-from qutebrowser import __version__
 
 # pyright: reportUndefinedVariable=false
 
@@ -59,13 +53,6 @@ config.unbind("d")
 config.bind("x", "tab-close")
 config.unbind("u")
 config.bind("X", "undo")
-if Version(__version__) <= Version("2.5.4"):
-    config.bind("d", "run-with-count 10 scroll down")
-    config.bind("u", "run-with-count 10 scroll up")
-else:
-    # cmd-run-with-count is the new command for qutebrowser > 2.5.4
-    config.bind("d", "cmd-run-with-count 10 scroll down")
-    config.bind("u", "cmd-run-with-count 10 scroll up")
 
 
 # fake-key to manipulate insert mode

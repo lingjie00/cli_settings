8eb9f6719281814371d1308a1e525c6d5755589d
ling <64540764+lingjie00@users.noreply.github.com>
2024-06-09 18:37:52 +0800

    feat(nvim): Update init.lua for better plugin handling and theme setup
    This commit makes two major changes to the Neovim configuration. First,
    it moves the plugin synchronization to occur earlier in the bootstrap
    process. This ensures that plugins are installed before any other
    configurations are loaded.
    
    Second, it reorganizes the theme setup code. The colorscheme and
    highlighting settings are now set immediately after the plugin
    synchronization, ensuring that they are applied correctly. The
    termguicolors option has been removed as it was redundant.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,10 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 	vim.cmd([[packadd packer.nvim]])
 end
+if is_bootstrap then
+	-- install plugin before loading configs
+	require("packer").sync()
+end
 
 -- Install packages
 require("check")
@@ -13,9 +17,10 @@ require("plugin")
 -- personalised config
 require("keymap")
 require("set")
-if is_bootstrap then
-	require("packer").sync()
-end
+
+vim.cmd("colorscheme dracula")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
@@ -24,10 +29,3 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	group = packer_group,
 	pattern = vim.fn.expand("$MYVIMRC"),
 })
-
--- use wider colorscheme
-vim.opt.termguicolors = true
-
-vim.cmd("colorscheme dracula")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

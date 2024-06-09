115ff0adcbea8fa56402ff0f45064c4cb420e645
ling <64540764+lingjie00@users.noreply.github.com>
2024-06-09 18:47:53 +0800

    Revert 2 commits
    e0770f7 'feat(nvim): streamline plugin installation and color scheme setup'
    8eb9f67 'feat(nvim): Update init.lua for better plugin handling and theme setup'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,10 @@
 -- Install packer
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	is_bootstrap = true
 	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 	vim.cmd([[packadd packer.nvim]])
-	-- install plugin before loading configs
-	require("packer").sync()
-end
-if is_bootstrap then
-	-- install plugin before loading configs
-	require("packer").sync()
 end
 
 -- Install packages
@@ -17,10 +13,9 @@ require("plugin")
 -- personalised config
 require("keymap")
 require("set")
-
-vim.cmd("colorscheme dracula")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+if is_bootstrap then
+	require("packer").sync()
+end
 
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
@@ -29,3 +24,10 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	group = packer_group,
 	pattern = vim.fn.expand("$MYVIMRC"),
 })
+
+-- use wider colorscheme
+vim.opt.termguicolors = true
+
+vim.cmd("colorscheme dracula")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

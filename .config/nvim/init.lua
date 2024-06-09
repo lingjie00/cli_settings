e0770f72bfbb64df575df79b956bda6ec2d400d9
ling <64540764+lingjie00@users.noreply.github.com>
2024-06-09 18:41:51 +0800

    feat(nvim): streamline plugin installation and color scheme setup
    This commit makes two main changes to the Neovim configuration. First,
    it simplifies the plugin installation process by removing the
    `is_bootstrap` variable and directly requiring `packer` to sync after
    cloning the repository. This ensures that plugins are installed before
    loading configs.
    
    Second, it moves the color scheme setup to a more appropriate location
    in the file, removing the need for the `termguicolors` option. The
    `dracula` color scheme is set and the background for `Normal` and
    `NormalFloat` highlight groups is set to none.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,10 @@
 -- Install packer
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	is_bootstrap = true
 	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 	vim.cmd([[packadd packer.nvim]])
+	-- install plugin before loading configs
+	require("packer").sync()
 end
 
 -- Install packages
@@ -13,9 +13,10 @@ require("plugin")
 -- personalised config
 require("keymap")
 require("set")
-if is_bootstrap then
-	require("packer").sync()
-end
+
+vim.cmd("colorscheme dracula")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
@@ -24,10 +25,3 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	group = packer_group,
 	pattern = vim.fn.expand("$MYVIMRC"),
 })
-
--- use wider colorscheme
-vim.opt.termguicolors = true
-
-vim.cmd("colorscheme dracula")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

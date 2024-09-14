a09359199e2f3f9cc5140bb4514af63cd5c7a906
ling <64540764+lingjie00@users.noreply.github.com>
2024-09-14 19:57:41 +0800

    feat: update vim-slime target to neovim
    Changed the default slime target from tmux to neovim. Commented out the
    tmux-specific configurations to avoid conflicts. This update allows
    better integration with neovim for sending code to the terminal.

--- .config/nvim/plugin/vim_slime.lua
+++ .config/nvim/plugin/vim_slime.lua
@@ -1,13 +1,14 @@
 -- set tmux as the default slime target
-vim.g.slime_target = "tmux"
+vim.g.slime_target = "neovim"
+-- vim.g.slime_target = "tmux"
 -- use Ipython notation of %% for a new cell
 vim.g.slime_cell_delimiter = "^# %%"
 
 -- fix tmux auto spacing issue
 vim.g.slime_bracketed_paste = 1
 -- set default target location
-vim.g.slime_default_config = { socket_name = "default", target_pane = ":.1" }
-vim.g.slime_dont_ask_default = 1
+-- vim.g.slime_default_config = { socket_name = "default", target_pane = ":.1" }
+-- vim.g.slime_dont_ask_default = 1
 
 -- change keymapping
 vim.g.slime_no_mappings = 1

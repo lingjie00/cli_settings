ddf304db86d1aeb982bf130b0336490af147e24b
ling <64540764+lingjie00@users.noreply.github.com>
2024-10-18 23:56:27 +0800

    feat: update conform setup with dynamic python formatters
    - Changed the conform setup to dynamically select python formatters
      based on their availability.
    - Replaced static formatter list with a function that checks for
      "isort" and "black" availability before adding them to the list.
    - Updated the configuration to use the new dynamic formatter selection
      method.

--- .config/nvim/plugin/conform.lua
+++ .config/nvim/plugin/conform.lua
@@ -2,17 +2,25 @@
 if Internet == 1 then
 	require("mason-conform").setup({ automatic_installation = true })
 end
-require("conform").setup({
+local conform = require("conform")
+conform.setup({
 	formatters_by_ft = {
-		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
-		python = { "darker", "isort" },
-		yaml = { "yamlfix" },
-		latex = { "latexindent" },
-		fish = { "fish_indent" },
+		python = function(bufnr)
+			local formatters = {}
+			if conform.get_formatter_info("isort", bufnr).available then
+				table.insert(formatters, "isort")
+			end
+			if conform.get_formatter_info("black", bufnr).available then
+				table.insert(formatters, "black")
+			end
+			return formatters
+		end,
 	},
 	format_on_save = {
 		timeout_ms = 2500,
-		lsp_fallback = true,
+	},
+	default_format_opts = {
+		lsp_format = "fallback",
 	},
 })

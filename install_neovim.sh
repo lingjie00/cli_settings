f2f8e82bfe477de47fae4fa2b90ce0f1be3f0d28
ling <64540764+lingjie00@users.noreply.github.com>
2024-10-19 01:05:09 +0800

    feat: add script to install Neovim and ripgrep
    This script automates the installation of Neovim and ripgrep along with
    their dependencies. It fetches the latest release versions from GitHub,
    downloads the tarballs, extracts them, and creates symbolic links for
    easy access.
    
    - Fetches latest Neovim release version
    - Downloads and extracts Neovim tarball
    - Creates symbolic link for Neovim binary
    - Fetches latest ripgrep release version
    - Downloads and extracts ripgrep tarball
    - Creates symbolic link for ripgrep binary

---
+++ install_neovim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# require: curl, jq, tar
+
+# Installing neovim and its dependencies
+NEOVIM_VERSION=$(curl -sL https://api.github.com/repos/neovim/neovim/releases/latest | jq -r ".tag_name")
+NEOVIM_TARBALL=https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim-linux64.tar.gz
+
+RG_VERSION=$(curl -sL https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | jq -r ".tag_name")
+RG_FOLDERNAME=ripgrep-$RG_VERSION"-x86_64-unknown-linux-musl"
+RG_TARBALL=https://github.com/BurntSushi/ripgrep/releases/download/$RG_VERSION/$RG_FOLDERNAME".tar.gz"
+
+echo $NEOVIM_TARBALL
+curl -o /tmp/nvim-linux64.tar.gz -L $NEOVIM_TARBALL
+tar xzvf /tmp/nvim-linux64.tar.gz -C $HOME
+ln -s $HOME/nvim-linux64/bin/nvim /usr/local/bin
+
+echo $RG_TARBALL
+curl -o /tmp/rg-linux64.tar.gz -L $RG_TARBALL
+tar xzvf /tmp/rg-linux64.tar.gz -C $HOME
+ln -s $HOME/"$RG_FOLDERNAME"/rg /usr/local/bin

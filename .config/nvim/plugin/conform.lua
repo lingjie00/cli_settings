75cb619d55449d69263379b51aebbaf866665084
ling <64540764+lingjie00@users.noreply.github.com>
2024-10-18 23:58:25 +0800

    feat: remove mason-conform plugin
    This commit removes the mason-conform plugin from the Neovim configuration.
    The plugin was previously used to set up conform with automatic installation.
    The conform setup has been updated to work without mason-conform, ensuring
    formatters like isort and black are used for Python files.

--- .config/nvim/plugin/conform.lua
+++ .config/nvim/plugin/conform.lua
@@ -1,26 +1,23 @@
 -- set up conform
-if Internet == 1 then
-	require("mason-conform").setup({ automatic_installation = true })
-end
 local conform = require("conform")
 conform.setup({
-	formatters_by_ft = {
-		-- Conform will run multiple formatters sequentially
-		python = function(bufnr)
-			local formatters = {}
-			if conform.get_formatter_info("isort", bufnr).available then
-				table.insert(formatters, "isort")
-			end
-			if conform.get_formatter_info("black", bufnr).available then
-				table.insert(formatters, "black")
-			end
-			return formatters
-		end,
-	},
-	format_on_save = {
-		timeout_ms = 2500,
-	},
-	default_format_opts = {
-		lsp_format = "fallback",
-	},
+    formatters_by_ft = {
+        -- Conform will run multiple formatters sequentially
+        python = function(bufnr)
+            local formatters = {}
+            if conform.get_formatter_info("isort", bufnr).available then
+                table.insert(formatters, "isort")
+            end
+            if conform.get_formatter_info("black", bufnr).available then
+                table.insert(formatters, "black")
+            end
+            return formatters
+        end,
+    },
+    format_on_save = {
+        timeout_ms = 2500,
+    },
+    default_format_opts = {
+        lsp_format = "fallback",
+    },
 })

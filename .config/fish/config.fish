02c4af5c01eb664d011a4ac11851b666ab9dfde1
ling <64540764+lingjie00@users.noreply.github.com>
2025-01-09 16:46:03 +0800

    feat(config): remove unused venv and tableau functions
    The venv and tableau functions were removed from the fish config file
    as they are no longer needed. This simplifies the configuration and
    removes potential sources of confusion or errors.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,6 @@
 # evaluate homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-source $HOME/venv/ds/bin/activate.fish
-
-function venv --argument-names cmd --description "Activate Python venvs - Ling"
-    set --local venv_path $HOME/venv/
-    set --local activate_path bin/activate.fish
-    switch "$cmd"
-
-        case ls
-            ls $venv_path
-
-        case path
-            echo $venv_path
-
-        case \*
-            echo "activate $cmd venv"
-            source "$venv_path/$cmd/$activate_path"
-    end
-end
-
-function tableau --description "Open PygWalker Tableau"
-    streamlit run $HOME/github/pygwalker_streamlit/tableau.py
-end
-
 function git_base --description "Go to base of git repo"
     cd (git rev-parse --show-toplevel)
 end

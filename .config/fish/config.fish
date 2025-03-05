ff93b5af84d60d5683d25d3b355bca63a1dfff9c
ling <64540764+lingjie00@users.noreply.github.com>
2025-03-05 09:52:23 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,3 +33,17 @@ fzf --fish | source
 set -x EDITOR nvim
 
 fish_vi_key_bindings
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
